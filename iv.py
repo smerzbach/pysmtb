95d1063bd223457709609d4c34637efd8b2a59fa
Sebastian Merzbach <smerzbach@gmail.com>
2019-08-05 11:33:09 +0200

    - started switching to PyQt5 - rudimentary support for arbitrary sized images

--- iv.py
+++ iv.py
@@ -3,24 +3,73 @@
 Created on Thu Oct 18 19:24:05 2018
 
 @author: merzbach
-"""
-import numpy as np
-from matplotlib import pyplot as plt
 
-class iv:
+"""
+
+from datetime import datetime
+from functools import wraps
+from IPython import get_ipython
+import numpy as np
+import sys
+import traceback
+import types
+
+import PyQt5.QtCore as QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QApplication, QGridLayout, QHBoxLayout, QLabel, QLineEdit, QMainWindow, QShortcut, QVBoxLayout, QWidget
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+from matplotlib.transforms import Bbox
+
+from matplotlib import pyplot as plt
+import matplotlib.gridspec as gridspec
+
+'''
+def MyPyQtSlot(*args):
+    if len(args) == 0 or isinstance(args[0], types.FunctionType):
+        args = []
+    @QtCore.pyqtSlot(*args)
+    def slotdecorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                func(*args)
+            except:
+                print("Uncaught Exception in slot")
+                traceback.print_exc()
+        return wrapper
+    return slotdecorator
+'''
+
+class iv(QMainWindow, QApplication):
     zoom_factor = 1.1
     x_zoom = True
     y_zoom = True
     x_stop_at_orig = True
     y_stop_at_orig = True
     
-    def __init__(self, image, transposeFrames=False, transposeCollage=False, borderWidth=0):
+    def __init__(self, *args):
+        QMainWindow.__init__(self, parent=None)
+        timestamp = datetime.now().strftime("%y%m%d_%H%M%S")
+        self.setWindowTitle('iv ' + timestamp)
+        
+        shell = get_ipython()
+        shell.magic('%matplotlib qt')
+        
         # make input 4D (width x height x channels x images)
-        while len(image.shape) < 4:
-            image = np.reshape(image, image.shape + (1, ))
-        self.w, self.h, self.nc = image.shape[0 : 3]
-        self.border_width = borderWidth
-        self.image = image
+        self.images = args
+        for imind, image in enumerate(self.images):
+            while len(self.images[imind].shape) < 3:
+                self.images[imind] = np.reshape(self.images[imind], self.images[imind].shape + (1, ))
+        self.imind = 0 # currently selected image
+        self.nims = len(self.images)
+        self.w, self.h, self.nc = self.images[self.imind].shape[0 : 3]
+        self.border_width = 1
         self.scale = 1.
         self.gamma = 1.
         self.offset = 0.
@@ -29,13 +78,15 @@ class iv:
         self.onchange_autoscale = True
         self.per_image_scaling = True
         self.is_collage = False
-        self.transpose_collage = transposeCollage
-        self.transpose_frames = transposeFrames
-        self.imind = 0 # currently selected image
-        self.nims = image.shape[3]
-        self.fig, self.ax = plt.subplots()
+        self.transpose_collage = False
+        self.transpose_frames = False
+        
+        self.initUI()
+        
         self.ih = self.ax.imshow(np.zeros((self.w, self.h, 3)))
-        plt.tight_layout()
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+        #plt.tight_layout()
         self.updateImage()
         if self.onchange_autoscale:
             self.autoscale()
@@ -46,13 +97,122 @@ class iv:
         self.cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
         self.cid = self.fig.canvas.mpl_connect('button_release_event', self.onrelease)
         self.cid = self.fig.canvas.mpl_connect('motion_notify_event', self.onmotion)
-        self.cid = self.fig.canvas.mpl_connect('key_press_event', self.onkeypress)
-        self.cid = self.fig.canvas.mpl_connect('key_release_event', self.onkeyrelease)
+        self.cid = self.fig.canvas.mpl_connect('key_press_event', self.keyPressEvent)#onkeypress)
+        self.cid = self.fig.canvas.mpl_connect('key_release_event', self.keyReleaseEvent)#onkeyrelease)
         self.cid = self.fig.canvas.mpl_connect('scroll_event', self.onscroll)
+        self.alt = False
+        self.control = False
+        self.shift = False
         #plt.show(block=True)
         #plt.pause(10)
-        plt.show(block=False)
+        #plt.show(block=False)
+        self.show()
         
+    def notify(self, obj, event):
+        isex = False
+        try:
+            return QApplication.notify(self, obj, event)
+        except Exception:
+            isex = True
+            print("Unexpected Error")
+            print(traceback.format_exception(*sys.exc_info()))
+            return False
+        finally:
+            if isex:
+                self.quit()
+        
+    def initUI(self):
+        #self.fig = plt.figure(figsize = (10, 10))
+        #self.ax = plt.axes([0,0,1,1])#, self.gs[0])
+        
+        self.widget = QWidget()
+        
+        self.fig = Figure(dpi=100)
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas.setParent(self.widget)
+        
+        #self.ax = Axes(fig=self.fig, rect=[0,0,1,1])
+        self.ax = self.fig.add_subplot(111)
+        self.ax.set_position(Bbox([[0, 0], [1, 1]]))
+        self.ax.set_anchor('NW')
+        
+        self.uiLEScale = QLineEdit(str(self.scale))
+        self.uiLEScale.setMinimumWidth(200)
+        self.uiLEScale.editingFinished.connect(lambda: self.callbackLineEdit(self.uiLEScale))
+        self.uiLEGamma = QLineEdit(str(self.gamma))
+        self.uiLEGamma.setMinimumWidth(200)
+        self.uiLEGamma.editingFinished.connect(lambda: self.callbackLineEdit(self.uiLEGamma))
+        self.uiLEOffset = QLineEdit(str(self.offset))
+        self.uiLEOffset.setMinimumWidth(200)
+        self.uiLEOffset.editingFinished.connect(lambda: self.callbackLineEdit(self.uiLEOffset))
+        self.uiLabelModifiers = QLabel('')
+        
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.uiLabelModifiers)
+        vbox.addWidget(self.uiLEScale)
+        vbox.addWidget(self.uiLEGamma)
+        vbox.addWidget(self.uiLEOffset)
+        
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.canvas)
+        hbox.addLayout(vbox)
+        
+        self.widget.setLayout(hbox)
+        self.setCentralWidget(self.widget)
+        
+        # keyboard shortcuts
+        scaleShortcut = QShortcut(QKeySequence('Ctrl+Shift+a'), self.widget)
+        scaleShortcut.activated.connect(self.autoscale)
+        
+    def callbackLineEdit(self, ui):
+        if ui == self.uiLEScale:
+            tmp = self.scale
+            try:
+                tmp = float(self.uiLEScale.text())
+            except:
+                print('')
+            self.setScale(tmp)
+        elif ui == self.uiLEGamma:
+            tmp = self.gamma
+            try:
+                tmp = float(self.uiLEGamma.text())
+            except:
+                print('')
+            self.setGamma(tmp)
+        elif ui == self.uiLEOffset:
+            tmp = self.offset
+            try:
+                tmp = float(self.uiLEOffset.text())
+            except:
+                print('')
+            self.setOffset(tmp)
+    
+    '''
+    @MyPyQtSlot()
+    def slot_text(self):#, ui=None):
+        ui = self.uiLEScale
+        if ui == self.uiLEScale:
+            print('scale: ' + str(self.scale))
+            tmp = self.scale
+            try:
+                tmp = float(self.uiLEScale.text())
+            except ValueError:
+                print('error')
+                self.uiLEScale.setText(str(self.scale))
+            self.scale = tmp
+            self.updateImage()
+        elif ui == self.uiLEGamma:
+            print('gamma')
+        elif ui == self.uiLEOffset:
+            print('offset')
+    
+    def on_draw(self):
+        """ Redraws the figure
+        """
+        #self.axes.grid(self.grid_cb.isChecked())
+        self.canvas.draw()
+    '''
+    
     def print_usage(self):
         print(' ')
         print('hotkeys: ')
@@ -86,20 +246,20 @@ class iv:
         # autoscale between user-selected percentiles
         if self.autoscale_prctiles:
             if self.per_image_scaling:
-                min, max = np.percentile(self.image[:, :, :, self.imind], (self.prctile, 100 - self.prctile))
+                lower, upper = np.percentile(self.images[self.imind], (self.prctile, 100 - self.prctile))
             else:
-                min, max = np.percentile(self.image[:], (self.prctile, 100 - self.prctile))
+                limits = [np.percentile(image, (self.prctile, 100 - self.prctile)) for image in self.images]
+                lower = np.min([lims[0] for lims in limits])
+                upper= np.max([lims[1] for lims in limits])
         else:
             if self.per_image_scaling:
-                min = np.min(self.image[:, :, :, self.imind])
-                max = np.max(self.image[:, :, :, self.imind])
+                lower = np.min(self.images[self.imind])
+                upper = np.max(self.images[self.imind])
             else:
-                min = np.min(self.image[:])
-                max = np.max(self.image[:])
-        self.offset = min
-        self.scale = 1. / (max - min)
-        print('scale: %3.5f, offset: %3.5f' % (self.scale, self.offset))
-        self.updateImage()
+                lower = np.min([np.min(image) for image in self.images])
+                upper = np.max([np.max(image) for image in self.images])
+        self.setOffset(lower, False)
+        self.setScale(1. / (upper - lower), True)
         
     def collage(self):
         nc = int(np.ceil(np.sqrt(self.nims)))
@@ -185,8 +345,26 @@ class iv:
         if self.is_collage:
             self.collage()
         else:
-            self.ih.set_data(self.tonemap(self.image[:, :, :, self.imind]))
+            self.ih.set_data(self.tonemap(self.images[self.imind]))
         self.fig.canvas.draw()
+    
+    def setScale(self, scale, update=True):
+        self.scale = scale
+        self.uiLEScale.setText(str(self.scale))
+        if update:
+            self.updateImage()
+    
+    def setGamma(self, gamma, update=True):
+        self.gamma = gamma
+        self.uiLEGamma.setText(str(self.gamma))
+        if update:
+            self.updateImage()
+    
+    def setOffset(self, offset, update=True):
+        self.offset = offset
+        self.uiLEOffset.setText(str(self.offset))
+        if update:
+            self.updateImage()
     
     def onclick(self, event):
         if event.dblclick:
@@ -218,25 +396,28 @@ class iv:
             self.prev_delta_x = delta_x
             self.prev_delta_y = delta_y
     
-    def onkeypress(self, event):
-        if event.key == '?':
+    def keyPressEvent(self, event):
+    #def onkeypress(self, event):
+        key = event.key()
+        mod = event.modifiers()
+        if key == Qt.Key_Question: # ?
             self.print_usage()
-        elif event.key == 'a':
+        elif key == Qt.Key_A: # a
             # trigger autoscale
             self.autoscale()
             return
-        elif event.key == 'A':
+        elif key == Qt.Key_A and mod == Qt.Key_Shift: # A
             # toggle autoscale between user-selected percentiles or min-max
             self.autoscale_prctiles = not self.autoscale_prctiles
             self.autoscale()
             return
-        elif event.key == 'c':
+        elif key == Qt.Key_C:
             # toggle on-change autoscale
             self.onchange_autoscale = not self.onchange_autoscale
             print('on-change autoscaling is %s' % ('on' if self.onchange_autoscale else 'off'))
-        elif event.key == 'G':
+        elif key == Qt.Key_G:
             self.gamma = 1.
-        elif event.key == 'L':
+        elif key == Qt.Key_L:
             # update axes for single image dimensions
             if self.is_collage:
                 self.switch_to_single_image()
@@ -245,34 +426,37 @@ class iv:
                 self.is_collage = not self.is_collage
             # also disable per-image scaling limit computation
             self.per_image_scaling = not self.per_image_scaling
-        elif event.key == 'O':
+        elif key == Qt.Key_O:
             self.offset = 0.
-        elif event.key == 'p':
+        elif key == Qt.Key_P:
             self.per_image_scaling = not self.per_image_scaling
             print('per-image scaling is %s' % ('on' if self.per_image_scaling else 'off'))
             self.autoscale()
-        elif event.key == 'S':
+        elif key == Qt.Key_S:
             self.scale = 1.
-        elif event.key == 'Z':
+        elif key == Qt.Key_Z:
             # reset zoom
             self.ih.axes.autoscale(True)
-        elif event.key == 'alt':
+        elif key == Qt.Key_Alt:
             self.alt = True
+            self.uiLabelModifiers.setText('alt: %d, ctrl: %d, shift: %d' % (self.alt, self.control, self.shift))
             return
-        elif event.key == 'control':
+        elif key == Qt.Key_Control:
             self.control = True
+            self.uiLabelModifiers.setText('alt: %d, ctrl: %d, shift: %d' % (self.alt, self.control, self.shift))
             return
-        elif event.key == 'shift':
+        elif key == Qt.Key_Shift:
             self.shift = True
+            self.uiLabelModifiers.setText('alt: %d, ctrl: %d, shift: %d' % (self.alt, self.control, self.shift))
             return
-        elif event.key == 'left':
+        elif key == Qt.Key_Left:
             self.switch_to_single_image()
             self.imind = np.mod(self.imind - 1, self.nims)
             print('image %d / %d' % (self.imind + 1, self.nims))
             if self.onchange_autoscale:
                 self.autoscale()
                 return
-        elif event.key == 'right':
+        elif key == Qt.Key_Right:
             self.switch_to_single_image()
             self.imind = np.mod(self.imind + 1, self.nims)
             print('image %d / %d' % (self.imind + 1, self.nims))
@@ -283,30 +467,32 @@ class iv:
             return
         self.updateImage()
             
-    def onkeyrelease(self, event):
-        if event.key == 'shift':
+    def keyReleaseEvent(self, event):
+    #def onkeyrelease(self, event):
+        key = event.key()
+        if key == Qt.Key_Alt:
+            self.alt = False
+        elif key == Qt.Key_Control:
+            self.control = False
+        elif key == Qt.Key_Shift:
             self.shift = False
-        elif event.key == 'shift':
-            self.shift = False
-        elif event.key == 'shift':
-            self.shift = False
+        self.uiLabelModifiers.setText('alt: %d, ctrl: %d, shift: %d' % (self.alt, self.control, self.shift))
     
     def onscroll(self, event):
-        if event.key == 'ctrl+shift':
+        if self.control and self.shift:
             # autoscale percentiles
             self.prctile *= np.power(1.1, event.step)
             self.prctile = np.minimum(100, self.prctile)
             print('auto percentiles: [%3.5f, %3.5f]' % (self.prctile, 100 - self.prctile))
-            event.key = 'A'
-            self.onkeypress(event)
-        elif event.key == 'control':
+            self.autoscale_prctiles = True
+            self.autoscale()
+        elif self.control:
             # scale
-            self.scale *= np.power(1.1, event.step)
-            print('scale: %f' % self.scale)
-        elif event.key == 'shift':
+            #self.setScale(self.scale * np.power(1.1, event.step))
+            self.setScale(self.scale * np.power(1.1, event.step))
+        elif self.shift:
             # gamma
-            self.gamma *= np.power(1.1, event.step)
-            print('gamma: %f' % self.gamma)
+            self.setGamma(self.gamma * np.power(1.1, event.step))
         elif event.inaxes:
             # zoom when inside image axes
             factor = np.power(self.zoom_factor, -event.step)
