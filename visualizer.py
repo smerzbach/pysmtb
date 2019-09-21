74980c817e08ae49165b8162fbfe0d5ac299d88a
Sebastian Merzbach <smerzbach@gmail.com>
2019-09-21 05:30:36 +0200

    simplifications

--- visualizer.py
+++ visualizer.py
@@ -1,34 +1,26 @@
 from visdom import Visdom
 
 import numpy as np
-import os
-import datetime
 
 class Plotter(object):
     """Plots to Visdom"""
     def __init__(self, envName='main', logFilename=None, port=8097):
         self.viz = Visdom(port=port, log_to_filename=logFilename, env=envName)
         self.env = envName
-        self.plots = {}
+        self.windows = {}
         self.logFilename = logFilename
 
-    def plot(self, var_name, split_name, title_name, x, y, semilogy=False, xlabel='epochs'):
+    def plot(self, windowName, legendName, x, y, ytype='linear', xlabel='', ylabel='', **kwargs):
         opts = {
-                'legend': [split_name],
-                'title': title_name,
+                'legend': [legendName],
+                'title': windowName,
                 'xlabel': xlabel,
-                'ylabel': var_name
+                'ylabel': ylabel,
+                'ytype': ytype,
             }
-        if semilogy:
-            opts['layoutopts'] = {
-                'plotly': {
-                        'yaxis': {
-                                'type': 'log',
-                                'autorange': True,
-                        }
-                    }
-                }
-        if var_name not in self.plots:
-            self.plots[var_name] = self.viz.line(X=np.array([x,x]), Y=np.array([y,y]), env=self.env, opts=opts)
+        for key in kwargs:
+            opts[key] = kwargs[key]
+        if windowName not in self.windows:
+            self.windows[windowName] = self.viz.line(X=np.array([x,x]), Y=np.array([y,y]), env=self.env, name=legendName, opts=opts)
         else:
-            self.viz.line(X=np.array([x]), Y=np.array([y]), env=self.env, win=self.plots[var_name], name=split_name, update = 'append')
+            self.viz.line(X=np.array([x]), Y=np.array([y]), env=self.env, win=self.windows[windowName], name=legendName, update='append')
