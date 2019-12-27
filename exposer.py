a94317ee35ddc82a54a995342b9f434c9ef4dd2b
Sebastian Merzbach <smerzbach@gmail.com>
2019-12-27 22:32:50 +0100

    exposer: more controls

--- exposer.py
+++ exposer.py
@@ -9,59 +9,88 @@ Created on Thu Nov 28 21:12:15 2019
 import numpy as np
 
 import PyQt5
-import PyQt5.QtCore as QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QCheckBox, QFormLayout, QGridLayout, QHBoxLayout, QLabel, QLineEdit, QMainWindow, QSlider, QShortcut, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QCheckBox, QComboBox, QFormLayout, QHBoxLayout, QLabel, QLineEdit, QMainWindow, QPushButton, QSlider, QSpinBox, QTextEdit, QWidget
 
-class exposer(QMainWindow, QApplication):
-    def __init__(self, target, **kwargs):
-        QMainWindow.__init__(self, parent=None)
+class exposer(QWidget):
+    def __init__(self, target, props, *args, **kwargs):
+        super(exposer, self).__init__(*args, **kwargs)
         
         self.target = target
-        self.props = kwargs.get('props', None)
+        self.props = props
         
-        self.widget = QWidget()
         self.form = QFormLayout()
         
         # set defaults for omitted parameters
         for name in self.props:
+            if not 'args' in self.props[name]:
+                self.props[name]['args'] = dict()
+                
             if not 'type' in self.props[name]:
                 self.props[name]['type'] = int
+            
             if not 'style' in self.props[name]:
                 self.props[name]['style'] = 'edit'
+            
             if not 'limits' in self.props[name]:
                 self.props[name]['limits'] = []
-            if self.props[name]['style'] == 'slider' and not 'step' in self.props[name]:
+            
+            if (self.props[name]['style'] == 'slider' or self.props[name]['style'] == 'sliderEdit') and not 'step' in self.props[name]:
                 self.props[name]['step'] = 1
+            
+            if (self.props[name]['style'] == 'slider' or self.props[name]['style'] == 'sliderEdit') and not 'sliderArgs' in self.props[name]:
+                    self.props[name]['sliderArgs'] = self.props[name]['args']
+            
+            if self.props[name]['style'] == 'sliderEdit' and not 'editArgs' in self.props[name]:
+                    self.props[name]['editArgs'] = self.props[name]['args']
         
         # create controls
         self.controls = dict()
         for name in self.props:
             prop = self.props[name]
-            if prop['style'] == 'edit':
-                self.controls[name] = QLineEdit(str(getattr(self.target, name)))
-                self.controls[name].setMinimumWidth(200)
+            if prop['style'] == 'checkbox':
+                self.controls[name] = QCheckBox(name, **prop['args'])
+                self.controls[name].setChecked(bool(getattr(self.target, name)))
+                self.controls[name].stateChanged.connect(self.callback)
+                
+            elif prop['style'] == 'combobox':
+                self.controls[name] = QComboBox(**prop['args'])
+                self.controls[name].addItems(prop['limits'])
+                value = getattr(self.target, name)
+                index = [i for i, it in enumerate(prop['limits']) if it == value]
+                index = index[0]
+                self.controls[name].setCurrentIndex(index)
+                self.controls[name].currentIndexChanged.connect(self.callback)
+            
+            elif prop['style'] == 'edit':
+                self.controls[name] = QLineEdit(str(getattr(self.target, name)), **prop['args'])
                 self.controls[name].editingFinished.connect(self.callback)
                 
             elif prop['style'] == 'slider' or prop['style'] == 'sliderEdit':
                 limits = prop['limits']
                 step = prop['step']
+                
                 if len(limits) != 2:
                     raise Exception("slider limits must be specified as [min, max]!")
                 
-                self.controls[name] = QSlider(Qt.Horizontal)
+                self.controls[name] = QSlider(Qt.Horizontal, None, **prop['sliderArgs'])
                 self.controls[name].setSingleStep(step)
-                self.controls[name].setRange(0, (limits[1] - limits[0]) / step)
+                self.controls[name].setRange(limits[0] / step, limits[1] / step)
                 self.controls[name].setValue(getattr(self.target, name) / step)
                 self.controls[name].valueChanged.connect(self.callback)
                 
-                if prop['style'] == 'sliderEdit':
-                    self.controls[name].sibling = QLineEdit(str(getattr(self.target, name)))
-                    self.controls[name].sibling.setMinimumWidth(200)
+                if self.props[name]['style'] == 'sliderEdit':
+                    self.controls[name].sibling = QLineEdit(str(getattr(self.target, name)), **prop['editArgs'])
                     self.controls[name].sibling.editingFinished.connect(self.callback)
                     self.controls[name].sibling.sibling = self.controls[name]
                     self.controls[name].sibling.tag = name
             
+            elif prop['style'] == 'spinbox':
+                self.controls[name] = QSpinBox(**prop['args'])
+                self.controls[name].setRange(prop['limits'][0], prop['limits'][1])
+                self.controls[name].setValue(prop['type'](getattr(self.target, name)))
+                self.controls[name].valueChanged.connect(self.callback)
+                
             self.controls[name].tag = name # store property name in control for access from the callback
             
             # add things into layout(s)
@@ -70,14 +99,14 @@ class exposer(QMainWindow, QApplication):
                 layout.addWidget(self.controls[name].sibling)
                 layout.addWidget(self.controls[name])
                 self.form.addRow(QLabel(name + ':'), layout)
-                print(layout.stretch(0), layout.stretch(1))
+                
+            elif prop['style'] == 'checkbox':
+                self.form.addRow(QLabel(''), self.controls[name])
+                
             else:
                 self.form.addRow(QLabel(name + ':'), self.controls[name])
         
-        hbox = QHBoxLayout()
-        hbox.addLayout(self.form)
-        self.widget.setLayout(hbox)
-        self.setCentralWidget(self.widget)
+        self.setLayout(self.form)
         self.show()
         
     def callback(self):
@@ -85,14 +114,22 @@ class exposer(QMainWindow, QApplication):
             target = self.sender()
             name = target.tag
             prop = self.props[name]
-            if type(target) == PyQt5.QtWidgets.QSlider: #prop['style'] == 'slider':
-                value = target.value() * float(prop['step']) + prop['limits'][0]
+            #print(type(target))
+            
+            if isinstance(target, PyQt5.QtWidgets.QCheckBox):
+                value = target.isChecked()
+                
+            elif isinstance(target, PyQt5.QtWidgets.QComboBox):
+                value = target.currentText()
+                
+            elif isinstance(target, PyQt5.QtWidgets.QSlider):
+                value = target.value() * float(prop['step']) # + prop['limits'][0]
                 
                 if prop['style'] == 'sliderEdit':
                     # update edit box next to slider
                     self.controls[name].sibling.setText(str(value))
                 
-            elif type(target) == PyQt5.QtWidgets.QLineEdit: #prop['style'] == 'edit':
+            elif isinstance(target, PyQt5.QtWidgets.QLineEdit):
                 value = target.text()
                 value = prop['type'](value)
                 limits = prop['limits']
@@ -107,11 +144,16 @@ class exposer(QMainWindow, QApplication):
                 
                 if prop['style'] == 'sliderEdit':
                     # update both slider and edit box with the potentially clamped value
-                    self.controls[name].setValue((value - prop['limits'][0]) / prop['step'])
+                    self.controls[name].setValue(value / prop['step']) #(value - prop['limits'][0]) / prop['step'])
                     self.controls[name].sibling.setText(str(value))
                 else:
                     # update edit box with potentially clamped value
                     self.controls[name].setText(str(value))
+            
+            elif isinstance(target, PyQt5.QtWidgets.QSpinBox):
+                value = target.value()
+                value = prop['type'](value)
+                
             else:
                 print("else: ", type(target))
             # actual update of parameter in the target object
@@ -122,13 +164,38 @@ class exposer(QMainWindow, QApplication):
 class bla:
     def __init__(self):
         self.flup = 1
+        self.fluup = 1
         self.flap = 1.
         self.flop = 3.
         self.flip = 1.
+        self.flep = True
+        self.flyp = 'asdf'
         
 if __name__ == "__main__":
     b = bla()
-    e = exposer(b, props=dict(flup={'type': int,   'style': 'edit',   'limits': [0, 10]},
-                              flap={'type': float, 'style': 'edit',   'limits': [-np.inf, np.inf]},
-                              flop={'type': float, 'style': 'slider', 'limits': [1, 10],    'step': 2},
-                              flip={'type': float, 'style': 'sliderEdit', 'limits': [0.5, 2.5], 'step': 0.1}))
+    
+    win = QMainWindow()
+    win.setWindowTitle('foo')
+    
+    widget1 = QWidget()
+    widget2 = QTextEdit()
+    widget1.setMinimumSize(PyQt5.QtCore.QSize(200, 400))
+    
+    layout = QHBoxLayout()
+    layout.addWidget(widget2, 1)
+    layout.addWidget(widget1, 1)
+    
+    widget = QWidget()
+    widget.setLayout(layout)
+    win.setCentralWidget(widget)
+    
+    e = exposer(b, parent=widget1, props=dict(
+                              flup={'type': int,   'style': 'edit',       'limits': [0, 10]},
+                              fluup={'type': int,  'style': 'spinbox',    'limits': [0, 10]},
+                              flap={'type': float, 'style': 'edit',       'limits': [-np.inf, np.inf]},
+                              flop={'type': float, 'style': 'slider',     'limits': [1, 10],    'step': 2, 'args': {'tickInterval': 1, 'tickPosition': QSlider.TicksBelow}},
+                              flip={'type': float, 'style': 'sliderEdit', 'limits': [0.5, 2.5], 'step': 0.1, 'sliderArgs': {'tickInterval': 10, 'tickPosition': QSlider.TicksBelow}},
+                              flep={'type': bool,  'style': 'checkbox',   'limits': [False, True]},
+                              flyp={'type': str,   'style': 'combobox',   'limits': ['a', 'b', 'c', 'asdf', 'd']}))
+    
+    win.show()    
