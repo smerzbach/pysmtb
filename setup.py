0f3b86b512347d94c4a043dd21431fac7ccf9622
Sebastian Merzbach <smerzbach@gmail.com>
2022-01-02 00:10:01 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pysmtb",
-    version="0.1.00",
+    version="0.1.01",
     author="Sebastian Merzbach",
     author_email="smerzbach@gmail.com",
     description="python toolbox of (mostly) image-related helper / visualization functions",
