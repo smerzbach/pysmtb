9cab53e711f6d5a9cf4528065d2686356e8eeadf
Sebastian Merzbach <smerzbach@gmail.com>
2022-12-16 00:10:24 +0100

    btf: generalized reading, added some examples for decoding & texture dumping

--- pysmtb/btf.py
+++ pysmtb/btf.py
@@ -1,29 +1,32 @@
 import numpy as np
+from pathlib import Path
 from struct import unpack
+from typing import Tuple, Union
 
 from pysmtb.utils import Dct
 from pysmtb.geometry import spherical_to_cartesian, cartesian_to_plane_stereographic
 
 
-def read_ubo_btf_dfmf(filename: str, roi: dict = None):
-    """read BTF (Bidirectional Texture Function) file in Uni Bonn DFMF (decorrelated full matrix factorization) format
+def read_ubo_btf(filepath: Union[str, Path], roi: dict = None) -> Tuple[Dct, Dct]:
+    with open(filepath, 'rb') as fp:
+        signature = fp.read(10).decode()
 
-    see also: https://cg.cs.uni-bonn.de/en/projects/btfdbb/download/ubo2014/
+    if signature == '!DFMF08FCR':
+        return read_ubo_btf_dfmf(filepath, roi=roi)
+    elif signature == '!FMF12FCER':
+        return read_ubo_fmf(filepath, roi=roi)
+    else:
+        raise Exception('unsupported BTF format: ' + str(signature))
 
-    optionally applies cropping (and optional downsampling) directly after loading, specify roi as dictionary with
-    fields:
-    left, top, width, height[, xstride, ystride]
-    """
-    def uint32():
-        return unpack('1I', fp.read(4))[0]
 
-    fp = open(filename, 'rb')
-    signature = fp.read(10).decode()
-    if signature != '!DFMF08FCR':
-        raise Exception('BTF file %s has unknown header: %s' % (filename, signature))
+def read_uint32(fp):
+    return unpack('1I', fp.read(4))[0]
+
+
+def read_common_header(fp, signature: str) -> Dct:
     pos_start = fp.tell()
-    size = uint32()
-    version = uint32()
+    size = read_uint32(fp)
+    version = read_uint32(fp)
     meta = Dct()
     meta.measurement_setup = fp.read(80)
     meta.image_sensor = fp.read(80)
@@ -31,31 +34,31 @@ def read_ubo_btf_dfmf(filename: str, roi: dict = None):
     meta.ppmm = unpack('1f', fp.read(4))[0]
     meta.rgb_scale_factor = unpack('3f', fp.read(12))
     if version > 1:
-        meta.cosine_in_data = uint32()
+        meta.cosine_in_data = read_uint32(fp)
     else:
         meta.cosine_in_data = False
     if version > 2:
-        xml_size = uint32()
+        xml_size = read_uint32(fp)
         meta.xml = fp.read(xml_size)
     else:
         meta.xml = ''
 
     meta.num_channels = 3
     if version > 3:
-        meta.num_channels = uint32()
+        meta.num_channels = read_uint32(fp)
         meta.channel_names = []
         for ci in range(meta.num_channels):
-            s = uint32()
+            s = read_uint32(fp)
             meta.channel_names.append(fp.read(s))
     else:
-        meta.channel_names = ['R', 'G','B']
+        meta.channel_names = ['R', 'G', 'B']
     pos_end = fp.tell()
 
     if pos_start + size != pos_end:
-        raise Exception('error parsing meta data from file ' + filename)
+        raise Exception('error parsing meta data from file ' + fp.name)
 
     # read directional sampling
-    meta.num_views = uint32()
+    meta.num_views = read_uint32(fp)
     meta.num_lights_per_view = []
     meta.views_2d = []
     meta.views_3d = []
@@ -66,7 +69,7 @@ def read_ubo_btf_dfmf(filename: str, roi: dict = None):
         meta.views_3d.append(view_dir)
         view_dir = cartesian_to_plane_stereographic(*view_dir)
         meta.views_2d.append(view_dir)
-        num_lights = uint32()
+        num_lights = read_uint32(fp)
         light_dirs = np.fromfile(fp, dtype=np.float32, count=num_lights * 2).reshape(num_lights, 2)
         light_dirs = spherical_to_cartesian(light_dirs[:, 0], light_dirs[:, 1])
         light_dirs = np.array(cartesian_to_plane_stereographic(*light_dirs)).T
@@ -74,24 +77,41 @@ def read_ubo_btf_dfmf(filename: str, roi: dict = None):
         meta.num_lights_per_view.append(num_lights)
     meta.num_lights = np.max(meta.num_lights_per_view)
 
-    meta.width = uint32()
-    meta.height = uint32()
+    meta.width = read_uint32(fp)
+    meta.height = read_uint32(fp)
 
     if signature.endswith('R'):
-        meta.num_rotations = uint32()
-        meta.rotations = np.fromfile(fp, dtype=np.float32, count=meta.num_rotations * 3).reshape(meta.num_rotations, 3)
+        meta.num_rotations = read_uint32(fp)
+        meta.rotations = np.fromfile(fp, dtype=np.float32, count=meta.num_rotations * 3).reshape(meta.num_rotations,                                                                3)
         meta.rotations = meta.rotations.reshape((meta.num_rotations, 3, 3))
 
-    meta.num_components = uint32()
-    meta.color_model = unpack('1i', fp.read(4))[0]
-    meta.color_mean = unpack('3f', fp.read(12))
-    meta.color_transformation_matrix = np.fromfile(fp, dtype=np.float32, count=3 * 3).reshape(3, 3)
+    return meta
 
-    data = Dct()
-    data.S = []
-    data.U = []
-    data.SxV = []
-    for ci in range(meta.num_channels):
+
+def read_ubo_fmf(filepath: Union[str, Path], roi: dict = None) -> Tuple[Dct, Dct]:
+    with open(filepath, 'rb') as fp:
+        signature = fp.read(10).decode()
+        if signature != '!FMF12FCER':
+            raise Exception('%s: BTF file is not a FMF file, expected signature !FMF12FCER, got %s' % (str(filepath), signature))
+
+        data = Dct()
+        meta = read_common_header(fp=fp, signature=signature)
+        header_version = unpack('1B', fp.read(1))[0]
+
+        if header_version >= 1:
+            meta.dynamic_range_reduction_method = read_uint32(fp)
+
+        if header_version >= 2:
+            image_size = unpack('2I', fp.read(8))
+
+            if np.prod(image_size) > 0:
+                data.height_map = np.fromfile(fp, dtype=np.float16, count=np.prod(image_size)).reshape(image_size)
+
+        if header_version >= 3:
+            raise Exception('unsupported FMF format with header version ' + str(header_version))
+
+        meta.num_components = read_uint32(fp)
+
         scalar_size = unpack('1B', fp.read(1))[0]
         if scalar_size == 2:
             dtype = np.float16
@@ -99,13 +119,56 @@ def read_ubo_btf_dfmf(filename: str, roi: dict = None):
             dtype = np.float32
         else:
             dtype = np.float64
-        num_components = uint32()
-        num_rows = uint32()
-        num_cols = uint32()
-        data.S.append(np.fromfile(fp, dtype=dtype, count=num_components))
-        data.U.append(np.fromfile(fp, dtype=dtype, count=num_rows * num_components).reshape(num_rows, num_components))
-        data.SxV.append(np.fromfile(fp, dtype=dtype, count=num_cols * num_components).reshape(num_cols, num_components))
-    fp.close()
+        num_components = read_uint32(fp)
+        num_rows = read_uint32(fp)
+        num_cols = read_uint32(fp)
+        data.S = np.fromfile(fp, dtype=dtype, count=num_components)
+        data.U = np.fromfile(fp, dtype=dtype, count=num_rows * num_components).reshape(num_rows, num_components)
+        data.SxV = np.fromfile(fp, dtype=dtype, count=num_cols * num_components).reshape(num_cols, num_components)
+
+    return meta, data
+
+
+def read_ubo_btf_dfmf(filepath: Union[str, Path], roi: dict = None):
+    """read BTF (Bidirectional Texture Function) file in Uni Bonn DFMF (decorrelated full matrix factorization) format
+
+    see also: https://cg.cs.uni-bonn.de/en/projects/btfdbb/download/ubo2014/
+
+    optionally applies cropping (and optional downsampling) directly after loading, specify roi as dictionary with
+    fields:
+    left, top, width, height[, xstride, ystride]
+    """
+
+    with open(filepath, 'rb') as fp:
+        signature = fp.read(10).decode()
+        if signature != '!DFMF08FCR':
+            raise Exception('BTF file %s has unknown header: %s' % (filepath, signature))
+
+        meta = read_common_header(fp=fp, signature=signature)
+
+        meta.num_components = read_uint32(fp)
+        meta.color_model = unpack('1i', fp.read(4))[0]
+        meta.color_mean = unpack('3f', fp.read(12))
+        meta.color_transformation_matrix = np.fromfile(fp, dtype=np.float32, count=3 * 3).reshape(3, 3)
+
+        data = Dct()
+        data.S = []
+        data.U = []
+        data.SxV = []
+        for ci in range(meta.num_channels):
+            scalar_size = unpack('1B', fp.read(1))[0]
+            if scalar_size == 2:
+                dtype = np.float16
+            elif scalar_size == 4:
+                dtype = np.float32
+            else:
+                dtype = np.float64
+            num_components = read_uint32(fp)
+            num_rows = read_uint32(fp)
+            num_cols = read_uint32(fp)
+            data.S.append(np.fromfile(fp, dtype=dtype, count=num_components))
+            data.U.append(np.fromfile(fp, dtype=dtype, count=num_rows * num_components).reshape(num_rows, num_components))
+            data.SxV.append(np.fromfile(fp, dtype=dtype, count=num_cols * num_components).reshape(num_cols, num_components))
 
     if roi is not None:
         x0 = roi['left']
--- tests/test_btf.py
+++ tests/test_btf.py
@@ -1,34 +1,78 @@
+import imageio
 import numpy as np
+from pathlib import Path
 import requests
+from tqdm import tqdm
 
-from pysmtb.btf import read_ubo_btf_dfmf
+from pysmtb.btf import read_ubo_btf
+from pysmtb.image import write_openexr, tonemap
+
 
 def test_dfmf_loading():
     """simple test for UBO2014 BTF reading function"""
-    filename = '/tmp/sample.btf'
-    url = 'http://cg.cs.uni-bonn.de/fileadmin/btf/UBO2014/carpet/carpet05_W400xH400_L151xV151.btf'
-    ret = requests.get(url, stream=True, verify=False)
+    url = 'https://cg.cs.uni-bonn.de/btf/UBO2014/leather/leather11_W400xH400_L151xV151.btf'
+    urls = [
+        'https://cg.cs.uni-bonn.de/btf/UBO2014/leather/leather04_resampled_W400xH400_L151xV151.btf',
+        'https://cg.cs.uni-bonn.de/btf/UBO2014/leather/leather10_resampled_W400xH400_L151xV151.btf',
+        'https://cg.cs.uni-bonn.de/btf/UBO2014/leather/leather11_resampled_W400xH400_L151xV151.btf',
+    ]
 
-    with open(filename, 'wb') as fp:
-        fp.write(ret.raw.data)
+    for url in tqdm(urls, 'downloading & reading BTF files'):
+        filename = url.split('/')[-1].split('.')[0]
+        material_name = filename.split('_')[0]
+        filepath = Path('/tmp') / (filename + '.btf')
 
-    meta, data = read_ubo_btf_dfmf(filename)
-    assert hasattr(data, 'U')
-    assert hasattr(data, 'SxV')
-    assert meta.width == 400 and meta.height == 400
-    assert meta.num_lights == 151 and meta.num_views == 151
+        if not filepath.exists():
+            ret = requests.get(url, stream=True, verify=False)
+            with open(filepath, 'wb') as fp:
+                fp.write(ret.raw.data)
 
-    # test cropping
-    meta, data = read_ubo_btf_dfmf(filename, roi=dict(left=0, top=0, width=50, height=50))
-    assert meta.width == 50 and meta.height == 50
+        meta, data = read_ubo_btf(filepath)
 
-    assert data.U[0].shape[0] == 151 * 151
-    assert data.SxV[0].shape[0] == 50 * 50
+        # debug dump of height map
+        if 'height_map' in data:
+            write_openexr('/tmp/%s_heightmap.exr' % material_name, data.height_map, channels=['L'])
+            imageio.imwrite('/tmp/%s_heightmap.png' % material_name, tonemap(data.height_map, normalize=True, normalization_percentiles=(1.0, 99.0), as_uint8=True))
 
-    # ensure projected view directions are within unit circle
-    view_dirs_2d = np.array(meta.views_2d)
-    assert np.all(-1 <= view_dirs_2d.min()) and np.all(view_dirs_2d <= 1)
-    assert np.all(np.linalg.norm(view_dirs_2d, axis=1) <= 1)
+        assert hasattr(data, 'U')
+        assert hasattr(data, 'SxV')
+        assert meta.width == 400 and meta.height == 400
+        assert meta.num_lights == 151 and meta.num_views == 151
+
+        # debug dump of top LV texture
+        if isinstance(data.U, np.ndarray):
+            def decode_texture(lind: int = 0, vind: int = 0) -> np.ndarray:
+                ind = vind * meta.num_lights * 3 + lind * 3
+                texture = data.SxV @ data.U[np.r_[ind:ind+3], :].T
+                texture = texture.reshape(meta.height, meta.width, 3)
+
+                drr_eps = 0.0
+                if 'dynamic_range_reduction_method' in meta and meta.dynamic_range_reduction_method > 0:
+                    drr_eps = meta.get('drr_eps', 1e-5)
+                    drr_eps += meta.get('drr_offset', 0.0)
+
+                return np.exp(texture) - drr_eps
+
+            linds = [0, 20, 45, 50]
+            vinds = [0, 0, 0, 0]
+
+            for lind, vind in zip(linds, vinds):
+                texture = decode_texture(lind=lind, vind=vind)
+                write_openexr('/tmp/%s_texture_L=%03d_V=%03d.exr' % (material_name, lind, vind), texture, channels=['R', 'G', 'B'])
+                imageio.imwrite('/tmp/%s_texture_L=%03d_V=%03d.png' % (material_name, lind, vind), tonemap(texture, normalize=True, normalization_percentiles=(1.0, 99.0), as_uint8=True))
+
+        # test cropping
+        meta, data = read_ubo_btf(filepath, roi=dict(left=0, top=0, width=50, height=50))
+        assert meta.width == 50 and meta.height == 50
+
+        assert data.U[0].shape[0] == 151 * 151
+        assert data.SxV[0].shape[0] == 50 * 50
+
+        # ensure projected view directions are within unit circle
+        view_dirs_2d = np.array(meta.views_2d)
+        assert np.all(-1 <= view_dirs_2d.min()) and np.all(view_dirs_2d <= 1)
+        assert np.all(np.linalg.norm(view_dirs_2d, axis=1) <= 1)
+
 
 if __name__ == '__main__':
     test_dfmf_loading()
